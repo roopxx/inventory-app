dc10af08022e816dff1af0861ecf58ec461af388
roopxx <127219908+roopxx@users.noreply.github.com>
2024-06-03 15:00:46 +0530

    Add styling to the items view page

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -5,6 +5,7 @@ const Category = require("../models/category");
 exports.items_list = asyncHandler(async (req, res, next) => {
   const items = await Items.find({}).exec();
   res.render("items_view", {
+    title: "Items",
     items: items,
   });
 });
